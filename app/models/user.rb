7a5ac59e3374051b271fe06ccaf14e3c98117bf8
Areeb Ijaz <ijazareeb@yahoo.com>
2020-11-24 02:46:22 +1100

    added routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings, dependent: :destroy
-  has_many :memories through: :bookings, dependent: :
+  has_many :memories, through: :bookings, dependent: :destroy
   validates :username, :email, :first_name, :last_name, :phone_number, :address, :encrypted_password, presence: true
   validates :username, :email, uniqueness: true
   devise :database_authenticatable, :registerable,
