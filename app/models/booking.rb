5486dbbac52405a930fa2fc0d7451a465fa46f4b
Ian Chin <ianchinbv@gmail.com>
2020-11-23 17:59:44 +1100

    added validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+  belongs_to :users, :memories
+  validates :location, :status, :start_date, :end_date, :price, presence: true
 end
--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,3 +1,5 @@
 class Memory < ApplicationRecord
   belongs_to :user
+  validates :name, :description, :price, :duration, :location, :category, presence: true
+  validates :name, uniqueness: true
 end
