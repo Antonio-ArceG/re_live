d9cfd814142592e5746b364e08a458d6b72d7013
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-24 13:29:38 +1100

    updated memory model with configuration, updated seeds file

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,2 +1,3 @@
 class MemoriesController < ApplicationController
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+alex = User.new(username: 'a', email: 'email@email.com', first_name: 'alex', last_name: 'kelso', phone_number: 12345678, address: '123 Fake Street', password: 'abcdefgh')
+alex.save!
+
+memory = Memory.new(name: 'running on the moon', description: 'literally running on the moon', price: 10, duration: '10 minutes', location: 'the moon', category: 'adventure', creator: alex)
+memory.save!
