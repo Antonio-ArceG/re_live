f27acb175a312a991fe2fe40e2e9a392c15e712a
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-24 15:31:06 +1100

    Revert "take two trying to update code"
    This reverts commit f58c08c6179ee0236ee71f9be619995adf41f4fc.

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,12 +1,3 @@
 class MemoriesController < ApplicationController
-  def show
-    @memory = Memory.find(params[:id])
-  end
-
-  private
-
-  def memory_params
-    params.require(:memory).permit(:name, :description, :price, :duration, :location, :category)
-  end
-
+  
 end
