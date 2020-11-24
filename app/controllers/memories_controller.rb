12805cadd93617dad7a6017b8072cf82f1d36ab4
antonio arce <anton.arcegomez@gmail.com>
2020-11-25 10:53:38 +1100

    redid the views and controller

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,8 +1,29 @@
 class MemoriesController < ApplicationController
   def index
     @memories = Memory.all
-  end 
+  end
+
   def show
     @memory = Memory.find(params[:id])
+  end
+
+  def new
+    @memory = Memory.new
+  end
+
+  def create
+    @memory = Memory.new(memory_params)
+    @memory.creator = current_user
+    if @memory.save
+      redirect_to memories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:name, :description, :price, :duration, :location, :category, :creator_id)
   end
 end
