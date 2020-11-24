e453e2d3b5e531efb4080ec19e2be080171483a5
Areeb Ijaz <25960853+areebijaz@users.noreply.github.com>
2020-11-25 00:39:35 +1100

    Revert "Create memories"

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,22 +1,8 @@
 class MemoriesController < ApplicationController
-
-
-  def new
-    @memory = Memory.new
-  end
-
-  def create
-    @memory = Memory.new(memory_params)
-    @memory.creator = current_user
-    if @memory.save
-      redirect_to memories_path
-    else
-      render :new
-    end
-  end
-  private
-
-  def memory_params
-    params.require(:memory).permit(:name, :description, :price, :duration, :location, :category, :creator_id)
+  def index
+    @memories = Memory.all
+  end 
+  def show
+    @memory = Memory.find(params[:id])
   end
 end
