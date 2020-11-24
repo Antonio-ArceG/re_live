f58c08c6179ee0236ee71f9be619995adf41f4fc
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-24 15:19:21 +1100

    take two trying to update code

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,3 +1,12 @@
 class MemoriesController < ApplicationController
-  
+  def show
+    @memory = Memory.find(params[:id])
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:name, :description, :price, :duration, :location, :category)
+  end
+
 end
