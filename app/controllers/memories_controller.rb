d998e777212104af5e84b5e338c8b5479f4cbd28
antonio arce <anton.arcegomez@gmail.com>
2020-11-24 18:02:25 +1100

    ARCH: new/create added in mem controller, memory new view with form

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,12 +4,26 @@ class MemoriesController < ApplicationController
   end
 
   def new
+    @memory = Memory.new
   end
 
   def create
+    @memory = Memory.new(memory_params)
+    @memory.creator = current_user
+    if @memory.save
+      redirect_to memories_path
+    else
+      render :new
+    end
   end
 
   def show
     @memory = Memory.find(params[:id])
-    end
+  end
+
+  private
+
+  def memory_params
+    params.require(:memory).permit(:name, :description, :price, :duration, :location, :category, :creator_id)
+  end
 end
