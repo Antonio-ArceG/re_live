85411e3b61c5756d06cc063e8d3f3d31500bfb1d
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-25 16:12:29 +1100

    unpacked stashed changes into branch.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+    def new
+        # we need @memory in our `simple_form_for`
+        @memory = Memory.find(params[:memory_id])
+        @review = Review.new
+      end
 end
+ 
