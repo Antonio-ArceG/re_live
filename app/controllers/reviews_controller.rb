bd29e81c267f632ca75d193a8c99451aae5a0d00
Alexander Kelso <alexander.kelso@outlook.com>
2020-11-25 18:31:45 +1100

    updated reviews controller, reviews view and dashboard to support the addition of reviews. Have confirmed views can be created and saved. Now, just need to be able to view them.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,25 @@
 class ReviewsController < ApplicationController
-    def new
-        # we need @memory in our `simple_form_for`
-        @memory = Memory.find(params[:memory_id])
-        @review = Review.new
-      end
+  def new
+      # we need @booking in our `simple_form_for`
+      @booking = Booking.find(params[:booking_id])
+      @review = Review.new
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking_id = @booking.id
+    if @review.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
  
