4642dd1e88e15ba2b29830c0a30444b9858e6283
Ian Chin <ianchinbv@gmail.com>
2020-11-25 12:31:43 +1100

    XXXX

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @memory = Memory.find(params[:memory_id])
+  end
+
+  def create
+    @booking = booking.new(booking_params)
+    @booking.creator = current_user
+    if @booking.save
+      redirect_to memories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:location, :status, :price, :start_date, :end_date)
+  end
 end
