482808afecd752a5833a6f4747cbc3f5f39d471f
Areeb Ijaz <ijazareeb@yahoo.com>
2020-11-25 11:20:32 +1100

    Added router to my_memories page, created my_memories action in dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController 
     def index
-    end    
+      @my_bookings =  Booking.find_by(customer_id: current_user.id)
+    end
+    def my_memories
+      @my_memories = Memory.find_by(creator_id: current_user.id)
+    end        
 end
