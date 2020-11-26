e59bd3fc80e347570be4e9ceb194afc2466dd920
antonio arce <anton.arcegomez@gmail.com>
2020-11-26 15:22:06 +1100

    seeds and images sorted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   has_many :bookings, dependent: :destroy
   has_many :memories, through: :bookings, dependent: :destroy
   validates :email, :encrypted_password, presence: true
